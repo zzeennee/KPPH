b389eb8071f950e345dfaf8daa91ae5cdc5a66b4
zzeennee <96711823+zzeennee@users.noreply.github.com>
2023-07-16 14:56:03 +0900

    230716 pjh
    api 백단 설정

---
+++ api/kpph/src/main/java/com/kpph/api/KpphApplication.java
@@ -0,0 +1,13 @@
+package com.kpph.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpphApplication.class, args);
+	}
+
+}
---
+++ api/kpph/src/test/java/com/kpph/api/KpphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpph.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
